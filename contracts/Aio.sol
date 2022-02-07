e4c764e64bf2e1dd8eca60ae3d8f1447ebca8c2f
Violent-Idiot <panakalaryan@gmail.com>
2022-02-07 14:23:22 +0530

    Rewamp aio.sol

--- contracts/Aio.sol
+++ contracts/Aio.sol
@@ -1,64 +1,67 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "hardhat/console.sol";
-
-interface LendingPoolAddressesProvider {
-    function getLendingPool() external returns (address);
-}
-
-interface WethGateway {
-    function depositETH(
-        address lendingPool,
-        address onBehalfOf,
-        uint16 referralCode
-    ) external;
-}
-
-interface LendingPool {
+interface IAave {
     function deposit(
         address asset,
         uint256 amount,
         address onBehalfOf,
         uint16 referralCode
     ) external;
+
+    function withdraw(
+        address asset,
+        uint256 amount,
+        address to
+    ) external;
+
+    function borrow(
+        address asset,
+        uint256 amount,
+        uint256 interestRateMode,
+        uint16 referralCode,
+        address onBehalfOf
+    ) external;
+
+    function repay(
+        address asset,
+        uint256 amount,
+        uint256 rateMode,
+        address onBehalfOf
+    ) external;
 }
 
-contract Aio {
-    address public asset; //0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2;
-    uint256 public amount;
-    address public depositer;
-    mapping(address => uint256) bal;
+interface Itoken {
+    function approve(address, uint256) external;
 
-    //MAINNET
-    // LendingPoolAddressesProvider provider =
-    //     LendingPoolAddressesProvider(
-    //         0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9
-    //     );
-    // LendingPool lp = LendingPool(0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9);
-    // WethGateway wg = WethGateway(0xcc9a0B7c43DC2a5F023Bb9b738E45B0Ef6B06E04);
+    function transfer(address, uint256) external;
 
-    // KOVAN
-    LendingPoolAddressesProvider provider =
-        LendingPoolAddressesProvider(
+    function deposit() external payable;
+
+    function withdraw(uint256) external;
+
+    function balanceOf(address) external view returns (uint256);
+}
+
+interface LendingPoolAddressesProvider {
+    function getLendingPool() external returns (address);
+}
+
+contract Aave {
+    address constant weth = 0xd0A1E359811322d97991E03f863a0C30C2cF029C;
+
+    function deposit(uint256 amt) external payable {
+        LendingPoolAddressesProvider provider = LendingPoolAddressesProvider(
             0x88757f2f99175387aB4C6a4b3067c77A695b0349
         );
-    LendingPool lp = LendingPool(0xE0fBa4Fc209b4948668006B2bE61711b7f465bAe);
-    WethGateway wg = WethGateway(0xA61ca04DF33B72b235a8A28CfB535bb7A5271B70);
+        IAave aave = IAave(0xE0fBa4Fc209b4948668006B2bE61711b7f465bAe);
+        aave = IAave(provider.getLendingPool());
 
-    constructor(
-        // uint256 _amount;
-        address _asset // address _provider
-    ) payable {
-        // amount = _amount;
-        asset = _asset;
-        // provider = LendingPoolAddressesProvider(_provider);
-    }
+        address tokenaddr = weth;
 
-    function deposit() external payable {
-        lp = LendingPool(provider.getLendingPool());
-        // GETTING ETH FROM MSG.VALUE
-        wg.depositETH(address(lp), msg.sender, 0);
-        lp.deposit(asset, amount, msg.sender, 0);
+        Itoken token = Itoken(tokenaddr);
+        token.deposit{value: amt}();
+        token.approve(address(aave), amt);
+        aave.deposit(tokenaddr, amt, address(this), 0);
     }
 }
